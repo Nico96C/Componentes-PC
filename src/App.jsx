2be0de94e8afc6b070dd062fbee3faade5972a33
Nico96C <nicolas.cuello96@hotmail.com>
2024-03-30 14:25:34 -0300

    Iconos modo oscuro, imagenes banner y arreglo responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import Logo from "./svg/logo.svg";
 import intel from "./img/Intel.webp";
+import Placas from "./img/Placas.webp";
+import gaming from "./img/gaming.webp";
+import Razer from "./img/Razer.webp";
 import ShoppingCart from "./svg/shopping-cart.svg";
+import SolIcon from "./svg/sol.jsx";
+import LunaIcon from "./svg/luna.jsx";
 import "./App.css";
+import "./index.css";
 import SearchIcon from "./svg/busqueda.jsx";
 import UserIcon from "./svg/usuario.jsx";
 import { useState } from "react";
@@ -13,9 +19,15 @@ function App() {
     setExpanded(!expanded);
   };
 
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
     <>
-      <div>
+      <div className={isDarkMode ? "dark-mode" : "light-mode"}>
         <header className="Navbar">
           <div className="Container">
             <div className="Logo-Container">
@@ -26,7 +38,7 @@ function App() {
               <UserIcon color="white" className="user-icon" />
             </div>
 
-            <div className={`Search-Background ${expanded ? 'expanded' : ''}`}>
+            <div className={`Search-Background ${expanded ? "expanded" : ""}`}>
               <div className="Search-Container">
                 <input
                   className="Search"
@@ -40,46 +52,98 @@ function App() {
             </div>
 
             <div className="Cart-Button">
-              <img src={ShoppingCart} alt="Componentes PC CartIcon" className="Cart" />
+              <img
+                src={ShoppingCart}
+                alt="Componentes PC CartIcon"
+                className="Cart"
+              />
             </div>
+
+            <button className="Button-Mode" onClick={toggleDarkMode}>
+              {isDarkMode ? <SolIcon /> : <LunaIcon />}
+            </button>
           </div>
         </header>
 
         <nav className="All-Categories">
           <div className="Categories-Container">
-              <button direction="left" className="Displace__Content"></button>
-              <div className="Categories">
-                <div className="Category-Element">
-                  <span> VideoCard </span>
-                </div>
-                <div className="Category-Element">
-                  <span> Procesadores </span>
-                </div>
-                <div className="Category-Element">
-                  <span> Motherboard </span>
-                </div>
-                <div className="Category-Element">
-                  <span> Mouse </span>
-                </div>
+            <button direction="left" className="Displace__Content"></button>
+            <div className="Categories">
+              <div className="Category-Element">
+                <span> VideoCard </span>
               </div>
-              <button direction="right" className="Displace__Content"></button>
+              <div className="Category-Element">
+                <span> Procesadores </span>
+              </div>
+              <div className="Category-Element">
+                <span> Motherboard </span>
+              </div>
+              <div className="Category-Element">
+                <span> Mouse </span>
+              </div>
+            </div>
+            <button direction="right" className="Displace__Content"></button>
           </div>
         </nav>
 
-        <div className="Web-Container"> 
+        <div className="Web-Container">
           <h1 className="Web-Title"> Componentes de PC </h1>
           <div className="Carrousel-Seccion">
             <div className="flechaL"></div>
             <div className="Carrousel-Item">
               <section className="Carrousel-ImageContainer">
-                <img src={intel} alt="Intel-Image" className="Image-Corrousel" />
-                <img src={intel} alt="Intel-Image" className="Image-Corrousel" />
-                <img src={intel} alt="Intel-Image" className="Image-Corrousel" />
-                <img src={intel} alt="Intel-Image" className="Image-Corrousel" />
+                <div className="Carrousel-ImageWrapper">
+                  <img
+                    src={intel}
+                    alt="Intel-Image"
+                    className="Image-Corrousel"
+                  />
+                  <div className="Carrousel-Text">
+                    <h1>¡Marca Lider! Intel esta con nosotros</h1>
+                    <button>Explora</button>
+                  </div>
+                </div>
+                <div className="Carrousel-ImageWrapper">
+                  <img
+                    src={Placas}
+                    alt="Placas-Video-Image"
+                    className="Image-Corrousel"
+                  />
+                  <div className="Carrousel-Text">
+                    <h1>Mejores Placas de Video!</h1>
+                    <button>Ver Precios</button>
+                  </div>
+                </div>
+                <div className="Carrousel-ImageWrapper">
+                  <img
+                    src={gaming}
+                    alt="PC-Gamer-Image"
+                    className="Image-Corrousel"
+                  />
+                  <div className="Carrousel-Text">
+                    <h1>Armado Completo de PC Gamer</h1>
+                    <button>Empezar</button>
+                  </div>
+                </div>
+                <div className="Carrousel-ImageWrapper">
+                  <img
+                    src={Razer}
+                    alt="Razer-Image"
+                    className="Image-Corrousel"
+                  />
+                  <div className="Carrousel-Text">
+                    <h1>Razer! Todo para Gamers</h1>
+                    <button>Ver más</button>
+                  </div>
+                </div>
               </section>
             </div>
           </div>
-          <div>B</div>
+
+          <div className="Featured-Container">
+            <h2 className="Featured-Title"> ¡Lo mas destacado! </h2>
+            <div> </div>
+          </div>
           <div>C</div>
           <div>D</div>
           <div>E</div>
